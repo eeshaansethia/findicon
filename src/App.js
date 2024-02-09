e42931903e6460c9b1c5995208a77054e766c1f5
Eeshaan Sethia <eeshaans05@gmail.com>
2024-02-09 11:27:43 +0530

    Basic frontend created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import React, { useState } from 'react';
+import './styles/app.css';
+import logo from './assets/icon.png';
+import Axios from 'axios';
+
 const App = () => {
+  const [prompt, setPrompt] = useState('');
+  const [icon, setIcon] = useState('');
+
+  const getIcon = async () => {
+    console.log('Pressed!')
+  }
   return (
-    <div className="App">
-      <h1>Findicon</h1>
+    <div className="app">
+      <div className="app-left">
+        <div className="app-left-new">
+          <button className="app-left-new-button">
+            <i className="fa-regular fa-pen-to-square"></i>
+            New Prompt
+          </button>
+        </div>
+      </div>
+      <div className="app-right">
+        <h1 className="app-right-name">
+          <div className="app-right-name-image">
+            <img className="app-right-name-image-icon" src={logo} />
+          </div>
+          Findicon
+        </h1>
+        <div className="app-right-search">
+          <input className="app-right-search-input" type="text" placeholder="Type your prompt to get an icon.." value={prompt} onChange={(e) => {
+            setPrompt(e.target.value);
+          }} />
+          <button className="app-right-search-button" onClick={getIcon}>
+            <i className="fa-solid fa-search" style={{
+              fontSize: '1rem'
+            }}></i>
+          </button>
+        </div>
+
+      </div>
     </div>
   );
 }
